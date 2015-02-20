7d96262d0f8d915c8d5909feea33b2c02bcfe44f
Stefan Eissing <stefan.eissing@greenbytes.de>
2015-02-20 13:42:37 -0500

    No CR, CR 3130351 3152981 3320981: RT3724 Add asynchronous event processing
    Rebranding of SSL_ERROR_WANT_X509_LOOKUP as SSL_ERROR_WANT_EVENT, making
    event type to wait for visible in SSL->rwstate, letting TLS_SRP have its
    own event type instead of piggybacking on SSL_X509_LOOKUP.
    Adding task for decryption of client key exchange response
    Adding task for generating client certificate verify message
    Adding task for signing of server key exchange message
    
    Patch: ssl_want_event.diff
    Patch: ssl_task_kx_decrypt.diff
    Patch: ssl_task_cert_vrfy.diff
    Patch: ssl_cert_vfry.diff
    Patch: ssl_schedule_task.diff
    Patch: ssl_task_kx_sign.diff
    Original Author: Stefan Eissing (stefan.eissing@greenbytes.de)
    Akamai Status: published RT#3724
    (cherry picked from commit e4fa5107524bb5e6e4c79953d436b7e59ee6c5e2)
    
    Conflicts:
    
    	ssl/s3_clnt.c
    	ssl/s3_srvr.c
    	ssl/ssl.h
    	ssl/ssl_locl.h
    
    Added:
    	ssl/s2_lib.c
    	ssl/s2_clnt.c
    	ssl/s2_srvr.c
    	ssl/ssl2.h
    
    CR 3130351: Export ciphers are broken SSLv2/SSLv3
    
    Some minor logic errors in the asynchronous event processing code
    prevented export ciphers from working in SSLv2 and SSLv3.
    
    s3_srvr.c: s->s3->tmp.use_rsa_tmp = 0 should only be done during the
    SSL_ST_SW_KEY_EXCH_A state.
    
    s2_srvr.c: the key_length variable, which is supposed to be the full
    length of the key, was being overwritten by the result of the RSA
    decryption. This is fine for non-export ciphers, but with export
    ciphers, the encrypted key length is usually only 40-bits, and the
    rest of the key is in the clear. So, by setting key_length to be the
    decryption length, the wrong amount of data was being copied later on.
    Instead, leave the calculated key_length as is, and do the encrypted/
    decrypted length comparison properly.
    
    Original Author: tshort@akamai.com
    Reviewer: rsalz@akamai.com
    Akamai Status: to be published
    
    CR 3152981: Fix pod2man error in Akamai code
    
    commit 9d73ec13063cdc150e0f73053cbdac443e564419 ("Akamai: No CR:
    Add asynchronous event processing") introduced some new lists to
    various manual pages, but did not quite follow the pod syntax for
    doing so.  =item elements must be nested between =over and =back
    elements (to inset the list properly); this commit adds the missing
    =over and =back statements.
    
    Reviewer: tshort@akamai.com
    Akamai Status: to-be-published
    
    CR 3320981: zero out some stack variables in tests
    
    Malloc failure could cause control to jump to the cleanup handler
    which would then access uninitialized memory in these locals.
    
    Coverity IDs 14183, 14185
    
    Original Author: bkaduk@akamai.com
    Code Reviewer: tshort@akamai.com
    Akamai Status: to-be-published (in RT #3724)

---
+++ test/tasktest.c
@@ -0,0 +1,178 @@
+/*
+ * Copyright 2014, Akamai Technologies. All Rights Reserved.
+ * This file is distributed under the terms of the OpenSSL license.
+ * author: stefan.eissing@greenbytes.de
+ */
+
+#include "testharness.c"
+
+static const int SLEN = 1024;
+
+static TestContext *exp_tctx;
+static const int *exp_events;
+static int exp_len;
+static int call_idx;
+static pthread_t worker;
+
+typedef struct {
+    SSL *ssl;
+    int event;
+    SSL_task_ctx *ctx;
+    SSL_task_fn *fn;
+} task_ctx;
+
+static int set_expected(TestContext *tctx, const int *expected)
+{
+    exp_tctx = tctx;
+    call_idx = 0;
+    exp_events = expected;
+    exp_len = 0;
+    for (; expected && *expected; ++expected)
+        ++exp_len;
+    return (exp_len);
+}
+
+/* Refuse task execution, letting openssl handle it */
+static int refuse_task_cb(SSL *s, int event, SSL_task_ctx *ctx, SSL_task_fn *fn)
+{
+    if (exp_events != NULL && call_idx < exp_len)
+        (void)TESTEQINT(exp_tctx, exp_events[call_idx], event, "unexpected task event");
+    else {
+        char buffer[SLEN];
+        snprintf(buffer, SLEN, "got event %d as #%d, expected only %d in %s",
+                 event, call_idx, exp_len, (exp_tctx->s_ctx == s->ctx)? "server" : "client");
+        (void)TESTASSERT(exp_tctx, 0, buffer);
+    }
+    ++call_idx;
+    return (0); /* refuse task */
+}
+
+/* Executes task directly in this thread */
+static int direct_task_cb(SSL *s, int event, SSL_task_ctx *ctx, SSL_task_fn *fn)
+{
+    refuse_task_cb(s, event, ctx, fn);
+    fn(s, ctx);
+    return (1); /* accept task */
+}
+
+static void *delay_task1(void *c)
+{
+    task_ctx *ctx = (task_ctx *)c;
+
+    usleep(250000);
+    ctx->fn(ctx->ssl, ctx->ctx);
+
+    return (NULL);
+}
+
+/* Executes task asynchrounously in separate thread */
+static int async_task_cb(SSL *s, int event, SSL_task_ctx *ctx, SSL_task_fn *fn)
+{
+    refuse_task_cb(s, event, ctx, fn);
+
+    task_ctx *task = calloc(1, sizeof(task_ctx));
+    task->ssl = s;
+    task->event = event;
+    task->ctx = ctx;
+    task->fn = fn;
+
+    int rc = pthread_create(&worker, NULL, delay_task1, (void *)task);
+    if (rc) {
+        fprintf(stderr, "ERROR; return code from pthread_create() is %d\n", rc);
+        return (-1);
+    }
+    return (1); /* accept task */
+}
+
+static int test_server(TestContext *tctx, const char *variant, int *events, SSL_schedule_task_cb cb)
+{
+    char buffer[SLEN];
+    snprintf(buffer, SLEN, "test_task_cb(server, %s)", variant);
+    TESTCASE(tctx, buffer);
+
+    int exp_calls = set_expected(tctx, events);
+    SSL_CTX_set_schedule_task_cb(tctx->s_ctx, cb);
+
+    SSL *s_ssl = SSL_new(tctx->s_ctx);
+    SSL *c_ssl = SSL_new(tctx->c_ctx);
+    c_ssl->debug = tctx->debug;
+
+    (void)TESTASSERT(tctx, chatter(tctx, s_ssl, c_ssl, 1024) == 0, "data transfered");
+    SSL_free(s_ssl);
+    SSL_free(c_ssl);
+
+    (void)TESTEQINT(tctx, exp_calls, call_idx, "unexpected invocations");
+
+    SSL_CTX_set_schedule_task_cb(tctx->s_ctx, NULL);
+    SSL_CTX_set_schedule_task_cb(tctx->c_ctx, NULL);
+    return (tctx->failed);
+}
+
+static int test_client(TestContext *tctx, const char *variant, int *events, SSL_schedule_task_cb cb)
+{
+    char buffer[SLEN];
+    snprintf(buffer, SLEN, "test_task_cb(client, %s)", variant);
+    TESTCASE(tctx, buffer);
+
+    int exp_calls = set_expected(tctx, events);
+    SSL_CTX_set_schedule_task_cb(tctx->c_ctx, cb);
+
+    SSL *s_ssl = SSL_new(tctx->s_ctx);
+    SSL *c_ssl = SSL_new(tctx->c_ctx);
+    c_ssl->debug = tctx->debug;
+
+    (void)TESTASSERT(tctx, chatter(tctx, s_ssl, c_ssl, 1024) == 0, "data transfered");
+    SSL_free(s_ssl);
+    SSL_free(c_ssl);
+
+    (void)TESTEQINT(tctx, exp_calls, call_idx, "unexpected invocations");
+
+    SSL_CTX_set_schedule_task_cb(tctx->s_ctx, NULL);
+    SSL_CTX_set_schedule_task_cb(tctx->c_ctx, NULL);
+    return (tctx->failed);
+}
+
+int test_setup(void)
+{
+    return (1);
+}
+
+int test_run(TestContext *tctx)
+{
+    int ret = 0;
+
+    if (strcmp("sslv2", tctx->protocol) == 0) {
+        int ev1[] = { SSL_EVENT_KEY_EXCH_DECRYPT_DONE, 0 };
+        ret |= TESTRUN(tctx, test_server(tctx, "dummy",    ev1,  refuse_task_cb));
+        ret |= TESTRUN(tctx, test_client(tctx, "dummy",    NULL, refuse_task_cb));
+        ret |= TESTRUN(tctx, test_server(tctx, "direct",   ev1,  direct_task_cb));
+        ret |= TESTRUN(tctx, test_server(tctx, "stalling", ev1,  async_task_cb));
+    } else if (strcmp("sslv3", tctx->protocol) == 0) {
+        int ev1[] = { SSL_EVENT_KEY_EXCH_DECRYPT_DONE, 0 };
+        /* Enforce cipher with RSA key exchange */
+        (void)TESTASSERT(tctx, SSL_CTX_set_cipher_list(tctx->c_ctx, "AES256-SHA"), "Kx=RSA cipher set in client");
+        ret |= TESTRUN(tctx, test_server(tctx, "dummy",    ev1,  refuse_task_cb));
+        ret |= TESTRUN(tctx, test_client(tctx, "dummy",    NULL, refuse_task_cb));
+        ret |= TESTRUN(tctx, test_server(tctx, "direct",   ev1,  direct_task_cb));
+        ret |= TESTRUN(tctx, test_server(tctx, "stalling", ev1,  async_task_cb));
+    } else if (strcmp("tlsv1_2", tctx->protocol) == 0) {
+        int ev1[] = { SSL_EVENT_KEY_EXCH_MSG_SIGNED, 0 };
+        (void)TESTASSERT(tctx, SSL_CTX_set_cipher_list(tctx->c_ctx, "ECDHE-RSA-AES256-GCM-SHA384"), "Kx=ECDH cipher set in client");
+        /* Enforce cipher with ECDH key exchange */
+        ret |= TESTRUN(tctx, test_server(tctx, "dummy",    ev1,  refuse_task_cb));
+        ret |= TESTRUN(tctx, test_client(tctx, "dummy",    NULL, refuse_task_cb));
+        ret |= TESTRUN(tctx, test_server(tctx, "direct",   ev1,  direct_task_cb));
+        ret |= TESTRUN(tctx, test_server(tctx, "stalling", ev1,  async_task_cb));
+    } else { /* TLSv1 and TLSv1.1 */
+        int ev1[] = { SSL_EVENT_KEY_EXCH_MSG_SIGNED, 0 };
+        (void)TESTASSERT(tctx, SSL_CTX_set_cipher_list(tctx->c_ctx, "DHE-RSA-AES256-SHA"), "Kx=DH cipher set in client");
+        (void)TESTASSERT(tctx, SSL_CTX_set_cipher_list(tctx->s_ctx, "DHE-RSA-AES256-SHA"), "Kx=DH cipher set in server");
+        /* Enforce cipher with DH key exchange */
+        ret |= TESTRUN(tctx, test_server(tctx, "dummy",    ev1,  refuse_task_cb));
+        ret |= TESTRUN(tctx, test_client(tctx, "dummy",    NULL, refuse_task_cb));
+        ret |= TESTRUN(tctx, test_server(tctx, "direct",   ev1,  direct_task_cb));
+        ret |= TESTRUN(tctx, test_server(tctx, "stalling", ev1,  async_task_cb));
+    }
+
+    return (ret);
+}
