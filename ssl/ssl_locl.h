c735951c7057aa8521cb742befa10ddb12237871
Stefan Eissing <stefan.eissing@greenbytes.de>
2015-02-20 13:42:37 -0500

    No CR, CR 3130351 3152981: RT3724 Add asynchronous event processing
    Rebranding of SSL_ERROR_WANT_X509_LOOKUP as SSL_ERROR_WANT_EVENT, making
    event type to wait for visible in SSL->rwstate, letting TLS_SRP have its
    own event type instead of piggybacking on SSL_X509_LOOKUP.
    Adding task for decryption of client key exchange response
    Adding task for generating client certificate verify message
    Adding task for signing of server key exchange message
    
    Patch: ssl_want_event.diff
    Patch: ssl_task_kx_decrypt.diff
    Patch: ssl_task_cert_vrfy.diff
    Patch: ssl_cert_vfry.diff
    Patch: ssl_schedule_task.diff
    Patch: ssl_task_kx_sign.diff
    Original Author: Stefan Eissing (stefan.eissing@greenbytes.de)
    Akamai Status: published RT#3724
    (cherry picked from commit e4fa5107524bb5e6e4c79953d436b7e59ee6c5e2)
    
    Conflicts:
    
    	ssl/s3_clnt.c
    	ssl/s3_srvr.c
    	ssl/ssl.h
    	ssl/ssl_locl.h
    
    Added:
    	ssl/s2_lib.c
    	ssl/s2_clnt.c
    	ssl/s2_srvr.c
    	ssl/ssl2.h
    
    CR 3130351: Export ciphers are broken SSLv2/SSLv3
    
    Some minor logic errors in the asynchronous event processing code
    prevented export ciphers from working in SSLv2 and SSLv3.
    
    s3_srvr.c: s->s3->tmp.use_rsa_tmp = 0 should only be done during the
    SSL_ST_SW_KEY_EXCH_A state.
    
    s2_srvr.c: the key_length variable, which is supposed to be the full
    length of the key, was being overwritten by the result of the RSA
    decryption. This is fine for non-export ciphers, but with export
    ciphers, the encrypted key length is usually only 40-bits, and the
    rest of the key is in the clear. So, by setting key_length to be the
    decryption length, the wrong amount of data was being copied later on.
    Instead, leave the calculated key_length as is, and do the encrypted/
    decrypted length comparison properly.
    
    Original Author: tshort@akamai.com
    Reviewer: rsalz@akamai.com
    Akamai Status: to be published
    
    CR 3152981: Fix pod2man error in Akamai code
    
    commit 9d73ec13063cdc150e0f73053cbdac443e564419 ("Akamai: No CR:
    Add asynchronous event processing") introduced some new lists to
    various manual pages, but did not quite follow the pod syntax for
    doing so.  =item elements must be nested between =over and =back
    elements (to inset the list properly); this commit adds the missing
    =over and =back statements.
    
    Reviewer: tshort@akamai.com
    Akamai Status: to-be-published

--- ssl/ssl_locl.h
+++ ssl/ssl_locl.h
@@ -882,6 +882,7 @@ const SSL_METHOD *func_name(void)  \
                 ssl_undefined_void_function, \
                 ssl3_callback_ctrl, \
                 ssl3_ctx_callback_ctrl, \
+                ssl3_signal_event, \
         }; \
         return &func_name##_data; \
         }
@@ -919,6 +920,7 @@ const SSL_METHOD *func_name(void)  \
                 ssl_undefined_void_function, \
                 ssl3_callback_ctrl, \
                 ssl3_ctx_callback_ctrl, \
+                ssl3_signal_event, \
         }; \
         return &func_name##_data; \
         }
@@ -956,6 +958,7 @@ const SSL_METHOD *func_name(void)  \
         ssl_undefined_void_function, \
         ssl3_callback_ctrl, \
         ssl3_ctx_callback_ctrl, \
+        ssl3_signal_event, \
         }; \
         return &func_name##_data; \
         }
@@ -1031,6 +1034,7 @@ const SSL_METHOD *func_name(void)  \
                 ssl_undefined_void_function, \
                 ssl3_callback_ctrl, \
                 ssl3_ctx_callback_ctrl, \
+                ssl3_signal_event, \
         }; \
         return &func_name##_data; \
         }
@@ -1140,6 +1144,10 @@ long ssl2_callback_ctrl(SSL *s, int cmd, void (*fp) (void));
 long ssl2_ctx_callback_ctrl(SSL_CTX *s, int cmd, void (*fp) (void));
 int ssl2_pending(const SSL *s);
 long ssl2_default_timeout(void);
+void ssl_task_rsa_decrypt(SSL *s, SSL_rsa_decrypt_ctx *ctx);
+int ssl_schedule_task(SSL *s, int task_type, SSL_task_ctx *ctx, SSL_task_fn fn);
+int ssl_get_event_result(SSL *s);
+int ssl_event_did_succeed(SSL *s, int event, int *result);
 
 const SSL_CIPHER *ssl3_get_cipher_by_char(const unsigned char *p);
 int ssl3_put_cipher_by_char(const SSL_CIPHER *c, unsigned char *p);
@@ -1200,6 +1208,7 @@ int ssl3_pending(const SSL *s);
 void ssl3_record_sequence_update(unsigned char *seq);
 int ssl3_do_change_cipher_spec(SSL *ssl);
 long ssl3_default_timeout(void);
+int ssl3_signal_event(SSL *s, int event, int retcode);
 
 void ssl3_set_handshake_header(SSL *s, int htype, unsigned long len);
 int ssl3_handshake_write(SSL *s);
@@ -1287,6 +1296,7 @@ int ssl3_send_certificate_request(SSL *s);
 int ssl3_send_server_done(SSL *s);
 int ssl3_get_client_certificate(SSL *s);
 int ssl3_get_client_key_exchange(SSL *s);
+int ssl3_process_client_key_exchange(SSL *s);
 int ssl3_get_cert_verify(SSL *s);
 #  ifndef OPENSSL_NO_NEXTPROTONEG
 int ssl3_get_next_proto(SSL *s);
