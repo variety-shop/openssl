854426c15bd59dee0b23271ad44b24830d6009e2
Todd Short <tshort@akamai.com>
2015-03-25 10:11:33 -0400

    CR 1865260 2361612 3163801: TLSv1.2/High cipher support
    Enable TLSv1.2 ciphers, but don't mark them as HIGH nor FIPS so that
    those cipher configuration string options don't inadvertently include
    them in the cipher list. They need to be explicitly added.
    
    CR 1865260: Make the cipher list of openssl-6.101.3.2 the same as openssl 5.2.x.
    Perforce: 9811618 9816016 9816152 9818837
    Patch: .../ak-patches/patches/CR1865260_CR2361612.diff
    Original Author: Edward Dao (edao@akamai.com)
    Reviewer: shegde@akamai.com
    
    CR 2361612: (TLS-1.2) Enable TLS-1.2 ciphers.
    Perforce: 11697689 11635861
    Patch: .../ak-patches/patches/CR1865260_CR2361612.diff
    Original Author: Sujeeth Thirumalai (sthiruma@akamai.com)
    Reviewer: rsalz@akamai.com
    
    CR 3163801: Restore HIGH ciphers
    
    Ghost had a backwards compatibility issue with TLSv1.2 ciphers being
    added to the HIGH list; it inadvertently impacted their customers'
    configurations. So, the HIGH cipher list was reverted to not include
    TLSv1.2 ciphers. This is a drastic change in behavior for OpenSSL.
    This was done in CR 1865260 and CR 2361612.
    
    This change restores the HIGH ciphers to OpenSSL's value and adds
    an DEP-COMPS option for ghost to enable the behavior it wants:
    
    DEP-COMPS := openssl/enable/ghost-high
    
    Original Author: tshort@akamai.com
    Code Reviewer: bkaduk@akamai.com
    Akamai Status: internal-only

--- util/mk1mf.pl
+++ util/mk1mf.pl
@@ -310,6 +310,7 @@ $cflags.=" -DOPENSSL_NO_WEAK_SSL_CIPHERS"   if $no_weak_ssl;
 $cflags.=" -DOPENSSL_NO_BUF_FREELISTS" if $no_buf_freelists;
 $cflags.=" -DOPENSSL_NO_HEARTBEATS" if $no_heartbeats;
 $cflags.=" -DOPENSSL_NO_AKAMAI_ASYNC_RSALG" if $no_akamai_async_rsalg; #Akamai
+$cflags.=" -DOPENSSL_NO_AKAMAI_GHOST_HIGH" if $no_akamai_ghost_high; #Akamai
 $cflags.=" -DOPENSSL_NO_AKAMAI_CLIENT_CACHE" if $no_akamai_client_cache; #Akamai
 $cflags.=" -DOPENSSL_PSK" if $no_psk;
 $cflags.=" -DZLIB" if $zlib_opt;
@@ -1257,6 +1258,7 @@ sub read_options
 	        "no-buf-freelists" => \$no_buf_freelists,
 	        "no-heartbeats" => \$no_heartbeats,
 	        "no-akamai-async-rsalg" => \$no_akamai_async_rsalg, #Akamai
+	        "no-akamai-ghost-high" => \$no_akamai_ghost_high, #Akamai
 	        "no-akamai-client-cache" => \$no_akamai_client_cache, #Akamai
 	        "no-psk" => \$no_psk,
 		"no-rsax" => 0,
