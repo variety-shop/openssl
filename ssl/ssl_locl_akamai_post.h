7cf76749ecb8f3e274baf874dd05521a120ac142
Laszlo Kovacs <lkovacs@akamai.com>
2015-03-26 14:39:26 -0400

    CR 5163: RT3869 Add shared session lists in SSL_CTX
    Includes
    CR 3736922: Fix for shared session list locking issue
    CR 3278531: Move session lists to EX_DATA
    CR 3736922: pass proper type to CRYPTO_THREAD_read_lock()
    
    Upstream PR #3761/Issue #2397
    
    Support for shared session lists via SSL_CTX_share_session_cache().
    Added locking during the sharing and cleanup.
    
    Previously, we stored a session list structure in SSL_CTX ex_data
    that held head and tail pointers for the list linkage of SSL_SESSION
    objects; these lists could be shared across multiple SSL_CTX objects
    and its lock/reference count used to protect the lhash of sessions
    that remained in each SSL_CTX.  When assigning SSL_CTXes to share a
    session cache, plain pointer aliasing was used for the lhash structure,
    which led to inconsistent application of the requirement to hold the
    lock from the shared session_list object across accesses to it.
    
    By moving the lhash structure to the shared session_list object,
    the ownership and locking model are made more clear.
    
    The locking is updated to hold the session_list lock across all accesses
    to the list head/tail pointers, the session lhash, and the list
    structure; this includes several fixes for previously unprotected
    accesses.  Unfortunately, we cannot properly lock an SSL_CTX_ctrl()
    function used to get the number of sessions in the cache (via the
    lhash object), but this is deemed to be acceptabls since such information
    is currently only used to determine when to evict stale entries and
    maintain a cap on the size of the session cache; being somewhat flexible
    in the application of that cap should not have far-reaching consequences.
    
    Provide a convenience API to lock the session list, search the lhash for
    a matching session, take a reference on that session, and return it,
    so that intrusive locking changes are not needed at most call sites.
    
    We furthermore make the SSL_CTX_sessions() API useless, as the lock
    for the shared session cache is not exposed to library consumers;
    reflect this reality by always returning failure from that function.
    Provide an alternative for ghost's use that combines two operations
    used in printing statistics.
    
    We want to lock the lock pointer contained within the SSL_CTX, not
    attempt to treat the SSL_CTX* as a pthread_rwlock_t*.
    
    Sadly, the compiler cannot help us, as upstream declines to provide
    any typedef for CRYPTO_RWLOCK other than 'void'.
    
    Also supply a trivial smoke-test case in sslapitest, that uses the
    SSL_CTX_share_session_cache() and SSL_CTX_akamai_session_stats_bio() APIs.
    
    Original Author: tshort@akamai.com, bkaduk@akamai.com
    Code Reviewer: tshort@akamai.com, bkaduk@akamai.com
    Akamai Status: to-be-published (related to github issue #2397)

--- ssl/ssl_locl_akamai_post.h
+++ ssl/ssl_locl_akamai_post.h
@@ -23,12 +23,31 @@
 #ifndef HEADER_SSL_LOCL_AKAMAI_POST_H
 # define HEADER_SSL_LOCL_AKAMAI_POST_H
 
+struct ssl_ctx_session_list_st
+{
+    /*
+     * The head element of the list has s->prev as a sentinel that's the
+     * address of the session_cache_head field; likewise, the tail has
+     * s->next as a sentinel that's the address of the session_cache_tail field.
+     * These head and tail pointers themselves are either NULL or point to
+     * the corresponding valid SSL_SESSION object.
+     */
+    struct ssl_session_st *session_cache_head;
+    struct ssl_session_st *session_cache_tail;
+    LHASH_OF(SSL_SESSION) *sessions;
+    int references; /* number of SSL_CTX's holding a reference */
+    CRYPTO_RWLOCK *lock;
+};
+
 typedef struct ssl_ctx_ex_data_akamai_st SSL_CTX_EX_DATA_AKAMAI;
 
 struct ssl_ctx_ex_data_akamai_st
 {
     /* Akamai proprietary options */
     unsigned int options;
+
+    /* session list sharing */
+    struct ssl_ctx_session_list_st *session_list;
 };
 
 typedef struct ssl_ex_data_akamai_st SSL_EX_DATA_AKAMAI;
@@ -44,5 +63,17 @@ int SSL_CTX_get_ex_data_akamai_idx(void);
 SSL_CTX_EX_DATA_AKAMAI *SSL_CTX_get_ex_data_akamai(SSL_CTX* ctx);
 int SSL_get_ex_data_akamai_idx(void);
 SSL_EX_DATA_AKAMAI *SSL_get_ex_data_akamai(SSL* s);
+
+SSL_CTX_SESSION_LIST *SSL_CTX_get0_session_list(SSL_CTX* ctx);
+SSL_CTX_SESSION_LIST *SSL_CTX_SESSION_LIST_new(
+    unsigned long (*hash)(const SSL_SESSION *),
+    int (*cmp)(const SSL_SESSION *, const SSL_SESSION *));
+/* returns number of references, so 0 = freed */
+int SSL_CTX_SESSION_LIST_free(SSL_CTX_SESSION_LIST *l, SSL_CTX *ctx);
+int SSL_CTX_SESSION_LIST_up_ref(SSL_CTX_SESSION_LIST *l);
+SSL_SESSION *SSL_CTX_SESSION_LIST_get1_session(SSL_CTX *ctx, SSL_SESSION *key);
+/* We expose these two from ssl_lib.c to ssl_akamai.c */
+unsigned long SSL_SESSION_hash(const SSL_SESSION *a);
+int SSL_SESSION_cmp(const SSL_SESSION *a, const SSL_SESSION *b);
 
 #endif /* HEADER_SSL_LOCL_AKAMAI_POST_H */
