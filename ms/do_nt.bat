93da2b79d7aa75ff27d0e9b4d0494932f1e80c6c
Todd Short <tshort@akamai.com>
2015-03-31 16:45:17 -0400

    CR 37270 157157: Changes to the Windows build
    From the OpenSSL 1.0.1e diff
    
    For: ms/do_nt.bat util/pl/VC-32.pl
    CR 37270: Changes to the Windows build:
    Perforce: 555560
    Original Author: Laszlo Kovacs (lkovacs@akamai.com)
    Reviewer: N/A
    
    For: util/mk1mf.pl
    CR 157157: Changes to the Windows VC9 build:
    Perforce: 2639903
    Original Author: jegerton@akamai.com
    Reviewer: N/A
    
    Akamai Status: unpublished

--- ms/do_nt.bat
+++ ms/do_nt.bat
@@ -2,6 +2,7 @@
 perl util\mkfiles.pl >MINFO
 perl util\mk1mf.pl no-asm VC-NT >ms\nt.mak
 perl util\mk1mf.pl dll no-asm VC-NT >ms\ntdll.mak
+perl util\mk1mf.pl dll_lib no-asm VC-NT >ms\ntdll_lib.mak
 
 perl util\mkdef.pl libeay NT > ms\libeay32.def
 perl util\mkdef.pl ssleay NT > ms\ssleay32.def
