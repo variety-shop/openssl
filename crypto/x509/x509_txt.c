579f9f8d7097214ec611311103a117cec656e422
Laszlo Kovacs <lkovacs@akamai.com>
2015-03-31 17:06:21 -0400

    CR 3720062: Backport OCSP updates error codes/messages
    OCSP Updates: error codes and multiple certificates
    
    RT3877: Add X509 OCSP error codes and messages
    Add additional OCSP error codes for X509 verify usage
    
    RT3867: Support Multiple CA certs in ocsp app
    Add the ability to read multiple CA certs from a single file in the
    ocsp app.
    
    Update some missing X509 errors in documentation.
    
    Reviewed-by: Richard Levitte <levitte@openssl.org>
    Reviewed-by: Rich Salz <rsalz@openssl.org>
    (Merged from https://github.com/openssl/openssl/pull/941)
    (cherry picked from commit 3bb0f989b53c59c6d4527a0b5077dbb7fabe14b7)

--- crypto/x509/x509_txt.c
+++ crypto/x509/x509_txt.c
@@ -169,6 +169,14 @@ const char *X509_verify_cert_error_string(long n)
         return ("Certificate Transparency required, but no valid SCTs found");
     case X509_V_ERR_PROXY_SUBJECT_NAME_VIOLATION:
         return ("proxy subject name violation");
+#ifndef OPENSSL_NO_AKAMAI
+    case X509_V_ERR_OCSP_VERIFY_NEEDED:
+        return("OCSP verification needed");
+    case X509_V_ERR_OCSP_VERIFY_FAILED:
+        return("OCSP verification failed");
+    case X509_V_ERR_OCSP_CERT_UNKNOWN:
+        return("OCSP unknown cert");
+#endif
 
     default:
         /* Printing an error number into a static buffer is not thread-safe */
