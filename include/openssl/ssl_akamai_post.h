d320018a95b3908af869a34a0b68c95e5eacba6c
Laszlo Kovacs <lkovacs@akamai.com>
2015-03-31 16:01:17 -0400

    No CR: RT3874 Add certificate verify data to SSL struct
    Add app_verify_callback and app_verify_arg to the SSL structure and add
    SSL_SESSION_set_verify_result() API. The values are copied from the
    SSL_CTX into the SSL.
    No CR originally specified
    
    Includes
    CR 3278531: Move SSL app_verify_callback to EX_DATA
    CR 3624021: Update SSL_SESSION_set_verify_result()
    
    Original Author: tshort@akamai.com
    Code Reviewer: bkaduk@akamai.com
    Code Reviewer: chzhang@akamai.com
    Akamai Status: proprietary, although, now we might just push it

--- include/openssl/ssl_akamai_post.h
+++ include/openssl/ssl_akamai_post.h
@@ -64,6 +64,12 @@ __owur X509 *SSL_get0_peer_certificate(const SSL *s);
 
 int SSL_CTX_share_session_cache(SSL_CTX *a, SSL_CTX *b);
 
+void SSL_SESSION_set_verify_result(SSL_SESSION *ss, long arg);
+void SSL_set_cert_verify_callback(SSL *s,
+                                  int (*cb) (X509_STORE_CTX *, void *),
+                                  void *arg);
+void* SSL_get_cert_verify_arg(SSL *s);
+
 /* Replaces SSL_CTX_sessions() and OPENSSL_LH_stats_bio() for shared session cache. */
 void SSL_CTX_akamai_session_stats_bio(SSL_CTX *ctx, BIO *b);
 
