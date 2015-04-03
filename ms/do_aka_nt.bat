27c2bbe93e6619371b3eb977a6da7ce6a7db950b
Todd Short <tshort@akamai.com>
2015-04-03 15:07:40 -0400

    CR 2977418 2794251 3171851 3209781 3658883 3237951 3171821 3171821 3171821: Componentization
    Update default OpenSSL Configuration to have everything including
    SSLv2/v3 disabled.
    Fixup do_aka_nt.bat to deal with multiple arguments
    
    CR 2794251: Remove unused openssl options
    Perforce: 15270056
    Original Author: Todd Short (tshort@akamai.com)
    Reviewer: pbohman@akamai.com
    
    CR 2977418: Add support for Akamai OpenSSL componentization
    Perforce: 15270056
    Original Author: Todd Short (tshort@akamai.com)
    Reviewer: pbohman@akamai.com
    
    CR 3171851: Fix akamake componentization includes
    
    * Don't automatically include akamake-pre.mak files.
    * Put the mods to OpenSSL config into the enable akamake files.
    * Replace too-generic Q and D variables to avoid conflicts
    * Remove OPENSSL_OPTIONS: not used anymore
    * Remove OPENSSL-XXX-IN-USE: was only used for DEP-COMPS, and they are
      not needed
    * Tweak how OPENSSL-TARGET is generated
    
    CR 3209781: Enable MD4 capability
    
    For "customer-facing legacy" reasons, we need to be able to enable
    MD4. This akamake file does that.
    
    Original Author: tshort@akamai.com
    Code Reviewer: bkaduk@akamai.com
    Akamai Status: internal only
    
    (cherry picked from commit f9135f88de119530bf790562e04e8c8c04d17e52)
    
    CR 3658883: Enable RC2 and other ciphers
    
    As the master component, enable bf, cast, md4, psk, rc2, rc4, ripemd
    
    Original Author: tshort@akamai.com
    Code Reviewer: bkaduk@akamai.com
    Akamai Status: internal only
    
    CR 3237951: Enable PSK capability
    
    The upcoming Nevada network will use TLS-PSK for secure in-region
    communication, so we need to provide a mechanism by which it can
    be re-enabled.
    
    Original Author: bkaduk@akamai.com
    Code Reviewer: tshort@akamai.com
    Akamai Status: internal only
    
    (cherry picked from commit 9bf27f8aa148a130a2112a405a0c976ec999fa8d)
    
    CR 3171821: disable RC2 by default
    
    Add rc2 to the %disabled hash in Configure, but provide an
    openssl/enable/rc2 virtual component that can be used to reenable
    it if needed.
    
    Original Author: bkaduk@akamai.com
    Code Reviewer: tshort@akamai.com
    Akamai Status: Internal only
    
    CR 3171821: Disable RC4 by default
    
    Adhere to RFC 7465 in a forceful manner; non-customer facing services
    should not have a reason for RC4 to be present, even if we do need to
    retain it for some legacy services.
    
    For such legacy services, allow an opt-out via openssl/enable/rc4.
    
    Original Author: bkaduk@akamai.com
    Code Reviewer: tshort@akamai.com
    Akamai Status: Internal only
    
    CR 3171821: explicitly disable krb5 in Configure
    
    It is necessary to pass --with-krb5= in order to actually enable
    krb5, which we currently do not do, but there's no harm in being
    explicit that Akamai does not want to use the outdated KTLS
    algorithms.
    
    Original Author: bkaduk@akamai.com
    Code Reviewer: tshort@akamai.com
    Akamai Status: Internal only

--- ms/do_aka_nt.bat
+++ ms/do_aka_nt.bat
@@ -1,4 +1,13 @@
 perl util\mkfiles.pl >MINFO
 
-perl util\mk1mf.pl %1 no-asm VC-NT-%2 >ms\nt.mak
-perl util\mk1mf.pl %1 dll_lib no-asm VC-NT-%2 >ms\ntdll_lib.mak
+REM Put all the arguments, except the last one, into %args%
+SET args=
+SET last=
+:again
+SET args=%args% %last%
+SET last=%1
+SHIFT
+IF NOT [%1]==[] GOTO again
+
+perl util\mk1mf.pl %args% no-asm VC-NT-%last% >ms\nt.mak
+perl util\mk1mf.pl %args% dll_lib no-asm VC-NT-%last% >ms\ntdll_lib.mak
