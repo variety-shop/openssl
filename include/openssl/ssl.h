44c1b107d8fd9bb416c7a802ca5928adbe7b52ba
Todd Short <tshort@akamai.com>
2015-06-26 14:52:31 -0400

    CR 3278531: ABI compat: define SSL/SSL_CTX EX_DATA
    Define preliminary EX_DATA structures for SSL and SSL_CTX structures.
    Define functions to initialize and get the EX_DATA.
    Move all this to ssl_akamai.c and the various akamai header files.
    Add EX_DATA initialization in SSL_CTX_new function (because you have to
    call that first - and there's already some in there).
    Ad SSL_akamai_opt...() API to handle Akamai-specific options and to not
    use OpenSSL options space.
    
    Original Author: tshort@akamai.com
    Code Reviewer: bkaduk@akamai.com
    Akamai Status: proprietary

--- include/openssl/ssl.h
+++ include/openssl/ssl.h
@@ -60,6 +60,10 @@
 # include <openssl/symhacks.h>
 # include <openssl/ct.h>
 
+# ifndef OPENSSL_NO_AKAMAI
+#  include <openssl/ssl_akamai_pre.h>
+# endif
+
 #ifdef  __cplusplus
 extern "C" {
 #endif
@@ -2074,6 +2078,10 @@ __owur const struct openssl_ssl_test_functions *SSL_test_functions(void);
 # endif
 
 extern const char SSL_version_str[];
+
+# ifndef OPENSSL_NO_AKAMAI
+#  include <openssl/ssl_akamai_post.h>
+# endif
 
 /* BEGIN ERROR CODES */
 /*
