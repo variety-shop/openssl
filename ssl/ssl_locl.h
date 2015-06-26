0f61104ba03c4835842d9ce62d2108538b70605e
Todd Short <tshort@akamai.com>
2015-06-26 14:52:31 -0400

    CR 3278531 3278531: ABI compat: define SSL/SSL_CTX EX_DATA
    Define preliminary EX_DATA structures for SSL and SSL_CTX structures.
    Define functions to initialize and get the EX_DATA.
    Move all this to ssl_akamai.c and the various akamai header files.
    Add EX_DATA initialization in SSL_CTX_new function (because you have to
    call that first - and there's already some in there).
    Ad SSL_akamai_opt...() API to handle Akamai-specific options and to not
    use OpenSSL options space.
    
    Fix the EX_DATA new index function to start at 1, rather than 0 (which
    is reserved for "app_data").
    
    When rebasing, this should be placed at the start of the Akamai
    changes.
    
    Original Author: tshort@akamai.com
    Code Reviewer: bkaduk@akamai.com
    Akamai Status: proprietary
    
    CR 3278531: make links to ssl_*akamai*.h on windows
    
    The Windows build requires that these links are made early on; add
    the external-facing headers to EXHEADERS and the internal ones to HEADERS
    in ssl/Makefile.org.
    
    The previous code to do this used the += operator, which is not supported
    by nmake on windows.
    
    This can be squashed into commit ed88c3606060443622e567538b3192b6fa940bd5
    on rebase.
    
    Original Author: bkaduk@akamai.com
    Code Reviewer: tshort@akamai.com
    Akamai Status: internal-only

--- ssl/ssl_locl.h
+++ ssl/ssl_locl.h
@@ -165,6 +165,8 @@
 # include <openssl/ssl.h>
 # include <openssl/symhacks.h>
 
+# include "ssl_locl_akamai_pre.h"
+
 # ifdef OPENSSL_BUILD_SHLIBSSL
 #  undef OPENSSL_EXTERN
 #  define OPENSSL_EXTERN OPENSSL_EXPORT
@@ -934,6 +936,7 @@ const SSL_METHOD *func_name(void)  \
                 ssl3_callback_ctrl, \
                 ssl3_ctx_callback_ctrl, \
                 ssl3_signal_event, \
+                OPENSSL_SSL3_AKAMAI_FNS \
         }; \
         return &func_name##_data; \
         }
@@ -973,6 +976,7 @@ const SSL_METHOD *func_name(void)  \
                 ssl3_callback_ctrl, \
                 ssl3_ctx_callback_ctrl, \
                 ssl3_signal_event, \
+                OPENSSL_SSL3_AKAMAI_FNS \
         }; \
         return &func_name##_data; \
         }
@@ -1012,6 +1016,7 @@ const SSL_METHOD *func_name(void)  \
         ssl3_callback_ctrl, \
         ssl3_ctx_callback_ctrl, \
         ssl3_signal_event, \
+        OPENSSL_SSL23_AKAMAI_FNS \
         }; \
         return &func_name##_data; \
         }
@@ -1050,6 +1055,8 @@ const SSL_METHOD *func_name(void)  \
                 ssl_undefined_void_function, \
                 ssl2_callback_ctrl,     /* local */ \
                 ssl2_ctx_callback_ctrl, /* local */ \
+                ssl2_signal_event, \
+                OPENSSL_SSL2_AKAMAI_FNS \
         }; \
         return &func_name##_data; \
         }
@@ -1090,6 +1097,7 @@ const SSL_METHOD *func_name(void)  \
                 ssl3_callback_ctrl, \
                 ssl3_ctx_callback_ctrl, \
                 ssl3_signal_event, \
+                OPENSSL_DTLS1_AKAMAI_FNS \
         }; \
         return &func_name##_data; \
         }
@@ -1559,4 +1567,9 @@ void custom_exts_free(custom_ext_methods *exts);
 #  define dtls1_process_heartbeat SSL_test_functions()->p_dtls1_process_heartbeat
 
 # endif
+
+# ifndef OPENSSL_NO_AKAMAI
+#  include "ssl_locl_akamai_post.h"
+# endif
+
 #endif
