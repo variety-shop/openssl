6e92770a99ea71d94c9e839534e28b7d11af78af
Todd Short <tshort@akamai.com>
2015-06-26 14:52:31 -0400

    CR 3278531 3278531: ABI compat: define SSL/SSL_CTX EX_DATA
    Define preliminary EX_DATA structures for SSL and SSL_CTX structures.
    Define functions to initialize and get the EX_DATA.
    Move all this to ssl_akamai.c and the various akamai header files.
    Add EX_DATA initialization in SSL_CTX_new function (because you have to
    call that first - and there's already some in there).
    Ad SSL_akamai_opt...() API to handle Akamai-specific options and to not
    use OpenSSL options space.
    
    Fix the EX_DATA new index function to start at 1, rather than 0 (which
    is reserved for "app_data").
    
    When rebasing, this should be placed at the start of the Akamai
    changes.
    
    Original Author: tshort@akamai.com
    Code Reviewer: bkaduk@akamai.com
    Akamai Status: proprietary
    
    CR 3278531: make links to ssl_*akamai*.h on windows
    
    The Windows build requires that these links are made early on; add
    the external-facing headers to EXHEADERS and the internal ones to HEADERS
    in ssl/Makefile.org.
    
    The previous code to do this used the += operator, which is not supported
    by nmake on windows.
    
    This can be squashed into commit ed88c3606060443622e567538b3192b6fa940bd5
    on rebase.
    
    Original Author: bkaduk@akamai.com
    Code Reviewer: tshort@akamai.com
    Akamai Status: internal-only

---
+++ ssl/ssl_akamai_pre.h
@@ -0,0 +1,40 @@
+/* ssl/ssl_akamai_pre.h */
+/*
+ * Copyright (C) 2016 Akamai Technologies. ALL RIGHTS RESERVED.
+ * This code was originally developed by Akamai Technologies and
+ * contributed to the OpenSSL project under the terms of the Corporate
+ * Contributor License Agreement v1.0
+ */
+/*
+ * This file contains Akamai-specific changes to OpenSSL
+ * Most of this code was originally contained in other locations
+ * within OpenSSL, and was even contributed upstream.
+ *
+ * However, to keep OpenSSL as "pristine" as possible, and to make
+ * rebasing/merging easier, Akamai-specific code will be moved to
+ * separate files *where possible*.
+ *
+ * This file is included as part of <ssl.h> although parts of this will
+ * likely need to move to <ssl_locl_akamai_pre.h> when structures become
+ * opaque. This file is not meant to be included on its own!
+ *
+ * THIS FILE IS LOADED AT THE BEGINING OF SSL.H
+ */
+
+#ifndef HEADER_SSL_AKAMAI_PRE_H
+# define HEADER_SSL_AKAMAI_PRE_H
+
+# ifdef  __cplusplus
+extern "C" {
+# endif
+
+/* for extending protocol methods */
+struct ssl_akamai_method_st
+{
+};
+
+# ifdef  __cplusplus
+}
+# endif
+
+#endif /* HEADER_SSL_AKAMAI_PRE_H */
