25edfa45153dce00931521d6fd5c3a9c92ba562f
Todd Short <tshort@akamai.com>
2016-01-21 11:47:17 -0500

    CR 3278531 3546161: Move async func to ssl_akamai/EX_DATA
    CR 3278531: Move async func to ssl_akamai/EX_DATA
    
    Move async structures to EX_DATA
    Move definitions to ssl_akamai files.
    Adds OPENSSL_NO_AKAMAI_ASYNC option (but not to Confgiure)
    
    May be combined with commit 2ac5664 and commit 3bb06d1 upon rebase.
    
    Original Author: tshort@akamai.com
    Code Reviewer: bkaduk@akamai.com
    Akamai Status: proprietary
    
    CR 3546161: fix build with sslv2
    
    It was broken in commit 3148d391d9096c46e7641dd03c948b82418c46fb, that
    moved the async functionality to EX_DATA, changing the case of
    type(def)s from SSL_task_fn to SSL_TASK_FN and from
    SSL_rsa_decrypt_ctx to SSL_RSA_DECRYPT_CTX.
    
    It was further broken by commit
    95cd4870ce523639791a5bcccd6b43e6ea443a50 which pulled the iovec bits
    to a separate file and changed the type from ssl_bucket to SSL_BUCKET,
    
    Since sslv2 was disabled by default, test builds didn't pick up
    the breakage.
    
    Also update the types in the relevant man pages, for completeness.
    
    Original Author: bkaduk@akamai.com
    Code Reviewer: tshort@akamai.com
    Akamai Status: proprietary

--- ssl/ssl_locl_akamai_post.h
+++ ssl/ssl_locl_akamai_post.h
@@ -23,12 +23,56 @@
 #ifndef HEADER_SSL_LOCL_AKAMAI_POST_H
 # define HEADER_SSL_LOCL_AKAMAI_POST_H
 
+# ifndef OPENSSL_NO_AKAMAI_ASYNC
+typedef struct ssl_async_event_st SSL_ASYNC_EVENT;
+
+struct ssl_async_event_st
+{
+    int type;              /* event number that was signalled */
+    int result;            /* return code signalled */
+    int err_func;          /* option error information, see SSLerr() */
+    int err_reason;
+    const char *err_file;
+    int err_line;
+};
+
+typedef struct ssl_async_task_st SSL_ASYNC_TASK;
+
+struct ssl_async_task_st
+{
+    int type;              /* event that is expected to be signalled */
+    SSL *ssl_ref;          /* optional SSL* reference handed out to task */
+    union
+    {
+        SSL_RSA_DECRYPT_CTX rsa_decrypt;
+        SSL_KEY_EXCH_PREP_CTX kx_sign;
+    } ctx;                 /* context/closure handed out to task */
+};
+# endif /* OPENSSL_NO_AKAMAI_ASYNC */
+
 typedef struct ssl_ctx_ex_data_akamai_st SSL_CTX_EX_DATA_AKAMAI;
 
 struct ssl_ctx_ex_data_akamai_st
 {
     /* Akamai proprietary options */
     unsigned int options;
+
+# ifndef OPENSSL_NO_AKAMAI_ASYNC
+    /* AKAMAI ASYNC */
+    /*
+     * If the application wants to offload work during handshake from the
+     * I/O thread to somewhere else, it can register this callback for
+     * doing so.
+     */
+    SSL_schedule_task_cb schedule_task_cb;
+    /*
+     * Callback that performs the task of setting up the client certificate
+     * verification by invoking the given function f on the given SSL* s.
+     * The function f will call SSL_signal_event() with proper parameters
+     * in the end.
+     */
+    int (*setup_cert_verify_cb)(SSL *s, void(*f)(SSL *s));
+# endif /* OPENSSL_NO_AKAMAI_ASYNC */
 };
 
 typedef struct ssl_ex_data_akamai_st SSL_EX_DATA_AKAMAI;
@@ -37,6 +81,12 @@ struct ssl_ex_data_akamai_st
 {
     /* Akamai proprietary options */
     unsigned int options;
+
+# ifndef OPENSSL_NO_AKAMAI_ASYNC
+    /* information from last signalled event, SSL_signal_event() */
+    SSL_ASYNC_EVENT event;
+    SSL_ASYNC_TASK task;
+# endif /* OPENSSL_NO_AKAMAI_ASYNC */
 };
 
 /* Used to initialize and get the akamai EX_DATA structures in one fell swoop */
@@ -44,5 +94,14 @@ int SSL_CTX_get_ex_data_akamai_idx(void);
 SSL_CTX_EX_DATA_AKAMAI *SSL_CTX_get_ex_data_akamai(SSL_CTX* ctx);
 int SSL_get_ex_data_akamai_idx(void);
 SSL_EX_DATA_AKAMAI *SSL_get_ex_data_akamai(SSL* s);
+
+# ifndef OPENSSL_NO_AKAMAI_ASYNC
+/* Async support */
+int ssl3_get_client_hello_post_app(SSL *s, int retry_cert);
+int ssl_check_clienthello_tlsext_async(SSL *s);
+void ssl_task_rsa_decrypt(SSL *s, SSL_RSA_DECRYPT_CTX *ctx);
+int ssl_schedule_task(SSL *s, int task_type, SSL_TASK_CTX *ctx, SSL_TASK_FN fn);
+int ssl3_process_client_key_exchange(SSL *s);
+# endif /* OPENSSL_NO_AKAMAI_ASYNC */
 
 #endif /* HEADER_SSL_LOCL_AKAMAI_POST_H */
