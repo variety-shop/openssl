variety-shop/openssl

commit 3148d391d9096c46e7641dd03c948b82418c46fb

CR 3278531: Move async func to ssl_akamai/EX_DATA
Move async structures to EX_DATA
Move definitions to ssl_akamai files.
Adds OPENSSL_NO_AKAMAI_ASYNC option (but not to Confgiure)

May be combined with commit 2ac5664 and commit 3bb06d1 upon rebase.

Original Author: tshort@akamai.com
Code Reviewer: bkaduk@akamai.com
Akamai Status: proprietary

ssl/s2_srvr.c
@@ -241,7 +241,7 @@ int ssl2_accept(SSL *s)
                 goto end;
             /* fall through */
         case SSL2_ST_GET_CLIENT_MASTER_KEY_C:
-            if (!ssl_event_did_succeed(s,
+            if (!SSL_event_did_succeed(s,
                                        SSL_EVENT_KEY_EXCH_DECRYPT_DONE, 
                                        &ret))
                 goto end;
@@ -536,7 +536,8 @@ static int decrypt_client_master_key(SSL *s)
         SSLerr(SSL_F_GET_CLIENT_MASTER_KEY,SSL_R_LENGTH_TOO_SHORT);
         return -1;
     } else {
-        SSL_rsa_decrypt_ctx *ctx = &s->task.ctx.rsa_decrypt;
+        SSL_EX_DATA_AKAMAI* ex_data = SSL_get_ex_data_akamai(s);
+        SSL_rsa_decrypt_ctx *ctx = &ex_data->task.ctx.rsa_decrypt;
         ctx->src = &(p[s->s2->tmp.clear]);
         ctx->dest = &(p[s->s2->tmp.clear]);
         ctx->src_len = s->s2->tmp.enc;
@@ -563,6 +564,7 @@ static int process_client_master_key(SSL *s)
     unsigned char decrypt_good;
     int is_export = SSL_C_IS_EXPORT(s->session->cipher);
     unsigned char *p = (unsigned char *)s->init_buf->data + 10;
+    SSL_EX_DATA_AKAMAI* ex_data = SSL_get_ex_data_akamai(s);
     
     size_t j;
 
@@ -602,7 +604,7 @@ static int process_client_master_key(SSL *s)
      * decryption result is either the decrypted number of bytes or a
      * negative value if decryption failed.
      */
-    decrypt_good = constant_time_eq_int_8(s->task.ctx.rsa_decrypt.dest_len, (int)num_encrypted_key_bytes);
+    decrypt_good = constant_time_eq_int_8(ex_data->task.ctx.rsa_decrypt.dest_len, (int)num_encrypted_key_bytes);
     for (j = 0; j < num_encrypted_key_bytes; j++) {
         p[s->s2->tmp.clear + j] =
             constant_time_select_8(decrypt_good, p[s->s2->tmp.clear + j],

ssl/d1_srvr.c
@@ -358,7 +358,7 @@ int dtls1_accept(SSL *s)
 #endif
             }
             if (s->s3->tmp.sub_state == SSL3_ST_SUB_2) {
-                if (!ssl_event_did_succeed(s, SSL_EVENT_TLSEXT_SERVERNAME_READY, &ret))
+                if (!SSL_event_did_succeed(s, SSL_EVENT_TLSEXT_SERVERNAME_READY, &ret))
                     goto end;
                 ret = ssl3_get_client_hello_post_app(s, 0);
                 if (ret <= 0)

ssl/s2_clnt.c
@@ -831,9 +831,10 @@ static int client_certificate(SSL *s)
     }
 
     if (s->state == SSL2_ST_SEND_CLIENT_CERTIFICATE_C) {
+        SSL_EX_DATA_AKAMAI* ex_data = SSL_get_ex_data_akamai(s);
         s->state = SSL2_ST_SEND_CLIENT_CERTIFICATE_D;
         i = ssl_schedule_task(s, SSL_EVENT_SETUP_CERT_VRFY_DONE,
-                              &s->task.ctx, ssl2_setup_client_verify_msg);
+                              &ex_data->task.ctx, ssl2_setup_client_verify_msg);
         if (i < 0) {
             SSLerr(SSL_F_SSL3_SEND_CLIENT_VERIFY,SSL_R_SSL_HANDSHAKE_FAILURE);
             return -1; /* async key signing couldn't be started */
@@ -841,7 +842,7 @@ static int client_certificate(SSL *s)
     }
 
     /* Waiting for our setup task to complete */
-    if (!ssl_event_did_succeed(s, SSL_EVENT_SETUP_CERT_VRFY_DONE, &i))
+    if (!SSL_event_did_succeed(s, SSL_EVENT_SETUP_CERT_VRFY_DONE, &i))
         return i;
 
     return (ssl2_do_write(s));

ssl/s2_lib.c
@@ -431,11 +431,6 @@ long ssl2_ctx_callback_ctrl(SSL_CTX *ctx, int cmd, void (*fp) (void))
     return (0);
 }
 
-int ssl2_signal_event(SSL *s, int event, int retcode)
-{
-    return 1;
-}
-
 /*
  * This function needs to check if the ciphers required are actually
  * available

ssl/s3_clnt.c
@@ -3226,10 +3226,11 @@ int ssl3_send_client_key_exchange(SSL *s)
 int ssl3_send_client_verify(SSL *s)
 {
     int i;
+    SSL_EX_DATA_AKAMAI* ex_data = SSL_get_ex_data_akamai(s);
     if (s->state == SSL3_ST_CW_CERT_VRFY_A) {
         s->state = SSL3_ST_CW_CERT_VRFY_B;
         i = ssl_schedule_task(s, SSL_EVENT_SETUP_CERT_VRFY_DONE,
-                              &s->task.ctx, SSLv3_setup_client_verify_msg);
+                              &ex_data->task.ctx, SSLv3_setup_client_verify_msg);
         if (i < 0) {
             SSLerr(SSL_F_SSL3_SEND_CLIENT_VERIFY,SSL_R_SSL_HANDSHAKE_FAILURE);
             return i;
@@ -3238,7 +3239,7 @@ int ssl3_send_client_verify(SSL *s)
     /* s->state == SSL3_ST_CW_CERT_VRFY_B */
 		
     /* Waiting for our setup method to complete */
-    if (!ssl_event_did_succeed(s, SSL_EVENT_SETUP_CERT_VRFY_DONE, &i))
+    if (!SSL_event_did_succeed(s, SSL_EVENT_SETUP_CERT_VRFY_DONE, &i))
         return i;
 		
     return ssl_do_write(s);

ssl/s3_lib.c
@@ -4994,11 +4994,6 @@ int ssl3_peek(SSL *s, void *buf, int len)
     return ssl3_read_internal(s, buf, len, 1);
 }
 
-int ssl3_signal_event(SSL *s, int event, int retcode)
-{
-    return (1);
-}
-
 int ssl3_renegotiate(SSL *s)
 {
     if (s->handshake_func == NULL)

ssl/s3_srvr.c
@@ -417,7 +417,7 @@ int ssl3_accept(SSL *s)
 #endif
             }
             if (s->s3->tmp.sub_state == SSL3_ST_SUB_2) {
-                if (!ssl_event_did_succeed(s, SSL_EVENT_TLSEXT_SERVERNAME_READY, &ret)) {
+                if (!SSL_event_did_succeed(s, SSL_EVENT_TLSEXT_SERVERNAME_READY, &ret)) {
                     goto end;
                 }
                 ret = ssl3_get_client_hello_post_app(s, 0);
@@ -672,7 +672,7 @@ int ssl3_accept(SSL *s)
             /* fall through */
 
         case SSL3_ST_SR_KEY_EXCH_PROCESS:
-            if (!ssl_event_did_succeed(s, SSL_EVENT_KEY_EXCH_DECRYPT_DONE, &ret))
+            if (!SSL_event_did_succeed(s, SSL_EVENT_KEY_EXCH_DECRYPT_DONE, &ret))
                 goto end;
 
 #ifdef OPENSSL_NO_AKAMAI_ASYNC_RSALG
@@ -1809,7 +1809,7 @@ int ssl3_send_server_done(SSL *s)
     return ssl_do_write(s);
 }
 
-static int ssl3_send_server_key_exchange_prep_data(SSL *s, SSL_key_exch_prep_ctx *ctx)
+static int ssl3_send_server_key_exchange_prep_data(SSL *s, SSL_KEY_EXCH_PREP_CTX *ctx)
 {
     int kn, i, al;
     BIGNUM *r[4];
@@ -1823,7 +1823,7 @@ static int ssl3_send_server_key_exchange_prep_data(SSL *s, SSL_key_exch_prep_ctx
 
     if (1) { /* minimize changes by forcing an indent */
         memset(r, 0, sizeof(r));
-        memset(ctx, 0, sizeof(SSL_key_exch_prep_ctx));
+        memset(ctx, 0, sizeof(SSL_KEY_EXCH_PREP_CTX));
         ctx->type = s->s3->tmp.new_cipher->algorithm_mkey;
 
 #ifndef OPENSSL_NO_RSA
@@ -2162,7 +2162,7 @@ static int ssl3_send_server_key_exchange_prep_data(SSL *s, SSL_key_exch_prep_ctx
     return (-1);
 }
 
-static void ssl3_send_server_key_exchange_sign_data(SSL *s, SSL_key_exch_prep_ctx *ctx)
+static void ssl3_send_server_key_exchange_sign_data(SSL *s, SSL_KEY_EXCH_PREP_CTX *ctx)
 {
     int al;
     unsigned char *d = &ctx->msg[s->method->ssl3_enc->hhlen];
@@ -2280,21 +2280,22 @@ static void ssl3_send_server_key_exchange_sign_data(SSL *s, SSL_key_exch_prep_ct
 int ssl3_send_server_key_exchange(SSL *s)
 {
     int ret;
+    SSL_EX_DATA_AKAMAI* ex_data = SSL_get_ex_data_akamai(s);
     if (s->state == SSL3_ST_SW_KEY_EXCH_A) {
-        ret = ssl3_send_server_key_exchange_prep_data(s, &s->task.ctx.kx_sign);
+        ret = ssl3_send_server_key_exchange_prep_data(s, &ex_data->task.ctx.kx_sign);
         if (ret <= 0)
             return ret;
 
         s->state = SSL3_ST_SW_KEY_EXCH_B;
         ret = ssl_schedule_task(s, SSL_EVENT_KEY_EXCH_MSG_SIGNED,
-                                &s->task.ctx.kx_sign,
-                                (SSL_task_fn *)ssl3_send_server_key_exchange_sign_data);
+                                &ex_data->task.ctx.kx_sign,
+                                (SSL_TASK_FN *)ssl3_send_server_key_exchange_sign_data);
         if (ret < 0) {
             SSLerr(SSL_F_SSL3_SEND_SERVER_KEY_EXCHANGE,SSL_R_UNKNOWN_STATE);
             return ret;
         }
     }
-    if (!ssl_event_did_succeed(s, SSL_EVENT_KEY_EXCH_MSG_SIGNED, &ret))
+    if (!SSL_event_did_succeed(s, SSL_EVENT_KEY_EXCH_MSG_SIGNED, &ret))
         return ret;
 
     return (ssl_do_write(s));
@@ -2434,7 +2435,8 @@ int ssl3_get_client_key_exchange(SSL *s)
     if (alg_k & SSL_kRSA) {
         RSA *rsa = NULL;
         EVP_PKEY *pkey = NULL;
-        SSL_rsa_decrypt_ctx *ctx = NULL;
+        SSL_RSA_DECRYPT_CTX *ctx = NULL;
+        SSL_EX_DATA_AKAMAI* ex_data = SSL_get_ex_data_akamai(s);
 
         /* FIX THIS UP EAY EAY EAY EAY */
         if (s->s3->tmp.use_rsa_tmp) {
@@ -2492,7 +2494,7 @@ int ssl3_get_client_key_exchange(SSL *s)
         }
 
         /* preserve across invocation */
-        ctx = &s->task.ctx.rsa_decrypt;
+        ctx = &ex_data->task.ctx.rsa_decrypt;
         ctx->src = p;
         ctx->dest = p;
         ctx->src_len = n;
@@ -2516,7 +2518,7 @@ int ssl3_get_client_key_exchange(SSL *s)
             s->state = SSL3_ST_SR_KEY_EXCH_PROCESS;
 #endif /* OPENSSL_NO_AKAMAI_ASYNC_RSALG */
         i = ssl_schedule_task(s, SSL_EVENT_KEY_EXCH_DECRYPT_DONE,
-                              ctx, (SSL_task_fn*)ssl_task_rsa_decrypt);
+                              ctx, (SSL_TASK_FN*)ssl_task_rsa_decrypt);
         if (i < 0) {
             al = SSL_AD_DECRYPT_ERROR;
             SSLerr(SSL_F_SSL3_GET_CLIENT_KEY_EXCHANGE, SSL_R_DECRYPTION_FAILED);
@@ -3188,7 +3190,8 @@ int ssl3_process_client_key_exchange(SSL *s)
     unsigned char rand_premaster_secret[SSL_MAX_MASTER_KEY_LENGTH];
     unsigned char decrypt_good, version_good;
     size_t j;
-    SSL_rsa_decrypt_ctx *ctx = &s->task.ctx.rsa_decrypt;
+    SSL_EX_DATA_AKAMAI* ex_data = SSL_get_ex_data_akamai(s);
+    SSL_RSA_DECRYPT_CTX *ctx = &ex_data->task.ctx.rsa_decrypt;
 
 #ifndef OPENSSL_NO_RSA
     if (s->s3->tmp.new_cipher->algorithm_mkey & SSL_kRSA) {
@@ -3678,7 +3681,7 @@ int ssl3_send_server_certificate(SSL *s)
     return ssl_do_write(s);
 }
 
-void ssl_task_rsa_decrypt(SSL *s, SSL_rsa_decrypt_ctx *ctx)
+void ssl_task_rsa_decrypt(SSL *s, SSL_RSA_DECRYPT_CTX *ctx)
 {
     size_t outlen;
     int ret;

ssl/ssl.h
@@ -514,7 +514,6 @@ struct ssl_method_st {
     int (*ssl_version) (void);
     long (*ssl_callback_ctrl) (SSL *s, int cb_id, void (*fp) (void));
     long (*ssl_ctx_callback_ctrl) (SSL_CTX *s, int cb_id, void (*fp) (void));
-    int (*ssl_signal_event)(SSL *s, int event, int retcode);
 # ifndef OPENSSL_NO_AKAMAI
     struct ssl_akamai_method_st akamai;
 # endif /* !OPENSSL_NO_AKAMAI */
@@ -972,10 +971,6 @@ typedef int (*GEN_SESSION_CB) (const SSL *ssl, unsigned char *id,
 
 typedef struct ssl_comp_st SSL_COMP;
 
-typedef void SSL_task_ctx;
-typedef void SSL_task_fn(SSL *, SSL_task_ctx *ctx);
-typedef int (*SSL_schedule_task_cb)(SSL *ssl, int task_type, SSL_task_ctx *ctx, SSL_task_fn *fn);
-
 # ifndef OPENSSL_NO_SSL_INTERN
 
 struct ssl_comp_st {
@@ -1165,19 +1160,6 @@ struct ssl_ctx_st {
      */
     ENGINE *client_cert_engine;
 #  endif
-
-    /* 
-     * If the application wants to offload work during handshake from the
-     * I/O thread to somewhere else, it can register this callback for doing so.
-     */
-    SSL_schedule_task_cb schedule_task_cb;
-
-    /* 
-     * Callback that performs the task of setting up the client certificate
-     * verification by invoking the given function f on the given SSL* s.
-     * The function f will call SSL_signal_event() with proper parameters in the end.
-     */
-    int (*setup_cert_verify_cb)(SSL *s, void(*f)(SSL *s));
 
 #  ifndef OPENSSL_NO_TLSEXT
     /* TLS extensions servername callback */
@@ -1373,10 +1355,6 @@ void SSL_CTX_set_client_cert_cb(SSL_CTX *ctx,
                                                        EVP_PKEY **pkey));
 int (*SSL_CTX_get_client_cert_cb(SSL_CTX *ctx)) (SSL *ssl, X509 **x509,
                                                  EVP_PKEY **pkey);
-
-void SSL_CTX_set_schedule_task_cb(SSL_CTX *ctx, SSL_schedule_task_cb cb);
-SSL_schedule_task_cb SSL_CTX_get_schedule_task_cb(SSL_CTX *ctx);
-
 # ifndef OPENSSL_NO_ENGINE
 int SSL_CTX_set_client_cert_engine(SSL_CTX *ctx, ENGINE *e);
 # endif
@@ -1532,52 +1510,14 @@ int SSL_extension_supported(unsigned int ext_type);
 # define SSL_READING     3
 # define SSL_X509_LOOKUP 4
 
-# define SSL_MIN_EVENT                    1000
-/* client is deciding which cert to present - doesn't follow MIN */
-# define SSL_EVENT_X509_LOOKUP            SSL_X509_LOOKUP
-/* server is processing TLS SRP client hello */
-# define SSL_EVENT_SRP_CLIENTHELLO        1000
-/* server is waiting for decryption of key */
-# define SSL_EVENT_KEY_EXCH_DECRYPT_DONE  1001
-/* client is waiting for cert verify setup */
-# define SSL_EVENT_SETUP_CERT_VRFY_DONE   1002
-/* server is siging the message for key exchange */
-# define SSL_EVENT_KEY_EXCH_MSG_SIGNED    1003
-# ifndef OPENSSL_NO_AKAMAI
-/* tlsext servername has been processed */
-#  define SSL_EVENT_TLSEXT_SERVERNAME_READY 1004
-# endif
-
-/*
- * These will only be used when doing non-blocking IO or asynchronous
- * event handling is triggered by callbacks. 
- */
+/* These will only be used when doing non-blocking IO */
 # define SSL_want_nothing(s)     (SSL_want(s) == SSL_NOTHING)
 # define SSL_want_read(s)        (SSL_want(s) == SSL_READING)
 # define SSL_want_write(s)       (SSL_want(s) == SSL_WRITING)
-# define SSL_want_x509_lookup(s) (SSL_want(s) == SSL_EVENT_X509_LOOKUP)
-# define SSL_want_event(s)       ((SSL_want(s) >= SSL_MIN_EVENT) || SSL_want_x509_lookup(s))
+# define SSL_want_x509_lookup(s) (SSL_want(s) == SSL_X509_LOOKUP)
 
 # define SSL_MAC_FLAG_READ_MAC_STREAM 1
 # define SSL_MAC_FLAG_WRITE_MAC_STREAM 2
-
-typedef struct {
-    unsigned char *src;
-    unsigned char *dest;
-    size_t src_len;
-    int dest_len; /* can be <0 if decryption fails */
-    RSA *rsa;
-    int padding;
-} SSL_rsa_decrypt_ctx;
-
-typedef struct {
-    unsigned long type;
-    EVP_PKEY *pkey;
-    const EVP_MD *md;
-    int n;
-    unsigned char *msg;
-    unsigned char *msg_end;
-} SSL_key_exch_prep_ctx;
 
 # ifndef OPENSSL_NO_SSL_INTERN
 
@@ -1852,25 +1792,6 @@ struct ssl_st {
     unsigned alpn_client_proto_list_len;
 #  endif                        /* OPENSSL_NO_TLSEXT */
 
-    /* information from last signalled event, SSL_signal_event() */
-    struct {
-        int type;              /* event number that was signalled */
-        int result;            /* return code signalled */
-        int err_func;          /* option error information, see SSLerr() */
-        int err_reason;
-        const char *err_file;
-        int err_line;
-    } event;
-
-    struct {
-        int type;              /* event that is expected to be signalled */
-        SSL *ssl_ref;          /* optional SSL* reference handed out to task */
-        union {
-            SSL_rsa_decrypt_ctx rsa_decrypt;
-            SSL_key_exch_prep_ctx kx_sign;
-        } ctx;                 /* context/closure handed out to task */
-    } task;
-
 #ifndef OPENSSL_NO_AKAMAI
     int (*app_verify_callback)();
     char *app_verify_arg;
@@ -2091,11 +2012,9 @@ DECLARE_PEM_rw(SSL_SESSION, SSL_SESSION)
 # define SSL_ERROR_SSL                   1
 # define SSL_ERROR_WANT_READ             2
 # define SSL_ERROR_WANT_WRITE            3
-# define SSL_ERROR_WANT_EVENT            4 /* check s->rwstate/SSL_want() 
-                                            * to see which event */
-# define SSL_ERROR_WANT_X509_LOOKUP      SSL_ERROR_WANT_EVENT /* backward compat */
-# define SSL_ERROR_SYSCALL               5 /* look at error stack/return
-                                            * value/errno */
+# define SSL_ERROR_WANT_X509_LOOKUP      4
+# define SSL_ERROR_SYSCALL               5/* look at error stack/return
+                                           * value/errno */
 # define SSL_ERROR_ZERO_RETURN           6
 # define SSL_ERROR_WANT_CONNECT          7
 # define SSL_ERROR_WANT_ACCEPT           8
@@ -2386,11 +2305,6 @@ void SSL_CTX_tlsext_ticket_appdata_cbs(SSL_CTX *ctx,
                                        void *arg);
 #endif /* OPENSSL_NO_AKAMAI */
 int SSL_want(const SSL *s);
-int SSL_signal_event_result(SSL *s, int event, int result, int errfunc, int errreason, const char *file, int line);
-# define SSL_signal_event(s, event, retcode) \
-        SSL_signal_event_result(s, event, retcode, 0, 0, NULL, 0)
-# define SSL_signal_event_err(s, event, func, reason) \
-        SSL_signal_event_result(s, event, -1, func, reason, __FILE__, __LINE__)
 int SSL_clear(SSL *s);
 
 int SSL_CTX_set_ciphers_ex(SSL_CTX *,const char *str, unsigned long flags);
@@ -3119,7 +3033,6 @@ void ERR_load_SSL_strings(void);
 # define SSL_F_SSL_SET_WFD                                196
 # define SSL_F_SSL_SHUTDOWN                               224
 # define SSL_F_SSL_SRP_CTX_INIT                           313
-# define SSL_F_SSL_TASK_RSA_DECRYPT                       323
 # define SSL_F_SSL_UNDEFINED_CONST_FUNCTION               243
 # define SSL_F_SSL_UNDEFINED_FUNCTION                     197
 # define SSL_F_SSL_UNDEFINED_VOID_FUNCTION                244

commit 770448acccb3d9aabb64e30e7a940b0989405d9b

CR 3546161: fix build with sslv2
It was broken in commit 3148d391d9096c46e7641dd03c948b82418c46fb, that
moved the async functionality to EX_DATA, changing the case of
type(def)s from SSL_task_fn to SSL_TASK_FN and from
SSL_rsa_decrypt_ctx to SSL_RSA_DECRYPT_CTX.

It was further broken by commit
95cd4870ce523639791a5bcccd6b43e6ea443a50 which pulled the iovec bits
to a separate file and changed the type from ssl_bucket to SSL_BUCKET,

Since sslv2 was disabled by default, test builds didn't pick up
the breakage.

Also update the types in the relevant man pages, for completeness.

Original Author: bkaduk@akamai.com
Code Reviewer: tshort@akamai.com
Akamai Status: proprietary

doc/ssl/SSL_CTX_set_schedule_task_cb.pod
@@ -8,8 +8,8 @@ SSL_CTX_set_schedule_task_cb, SSL_CTX_get_schedule_task_cb - handle task callbac
 
  #include <openssl/ssl.h>
 
- typedef void SSL_task_fn(SSL *, SSL_task_ctx *ctx);
- typedef int (*SSL_schedule_task_cb)(SSL *ssl, int event_type, SSL_task_ctx *ctx, SSL_task_fn *fn);
+ typedef void SSL_TASK_FN(SSL *, SSL_task_ctx *ctx);
+ typedef int (*SSL_schedule_task_cb)(SSL *ssl, int event_type, SSL_task_ctx *ctx, SSL_TASK_FN *fn);
 
  void SSL_CTX_set_schedule_task_cb(SSL_CTX *ctx, SSL_schedule_task_cb cb);
  SSL_schedule_task_cb SSL_CTX_get_schedule_task_cb(SSL_CTX *ctx);

ssl/s2_pkt.c
@@ -117,15 +117,15 @@
 
 static int read_n(SSL *s, unsigned int n, unsigned int max,
                   unsigned int extend);
-static int n_do_ssl_write(SSL *s, const ssl_bucket *buckets, int count, int offset, int len);
-static int write_pending(SSL *s, const ssl_bucket * buckets, int count, unsigned int len);
+static int n_do_ssl_write(SSL *s, const SSL_BUCKET *buckets, int count, int offset, int len);
+static int write_pending(SSL *s, const SSL_BUCKET * buckets, int count, unsigned int len);
 static int ssl_mt_error(int n);
 
 /*
  * SSL 2.0 imlementation for SSL_read/SSL_peek - This routine will return 0
  * to len bytes, decrypted etc if required.
  */
-static int ssl2_read_internal(SSL *s, const ssl_bucket *buckets, size_t count, int peek)
+static int ssl2_read_internal(SSL *s, const SSL_BUCKET *buckets, size_t count, int peek)
 {
     int n;
     unsigned char mac[MAX_MAC_SIZE];
@@ -159,7 +159,7 @@ static int ssl2_read_internal(SSL *s, const ssl_bucket *buckets, size_t count, i
         else
             n = len;
 
-        ssl_bucket_cpy_in(buckets, count, s->s2->ract_data, n);
+        SSL_BUCKET_cpy_in(buckets, count, s->s2->ract_data, n);
         if (!peek) {
             s->s2->ract_data_length -= n;
             s->s2->ract_data += n;
@@ -321,13 +321,13 @@ static int ssl2_read_internal(SSL *s, const ssl_bucket *buckets, size_t count, i
 
 int ssl2_read(SSL *s, void *buf, int len)
 {
-    ssl_bucket bucket = { buf, len };
+    SSL_BUCKET bucket = { buf, len };
     return ssl2_read_internal(s, &bucket, 1, 0);
 }
 
 int ssl2_peek(SSL *s, void *buf, int len)
 {
-    ssl_bucket bucket = { buf, len };
+    SSL_BUCKET bucket = { buf, len };
     return ssl2_read_internal(s, &bucket, 1, 1);
 }
 
@@ -447,7 +447,7 @@ int ssl2_write(SSL *s, const void *_buf, int len)
 {
     const unsigned char *buf=_buf;
     unsigned int n, tot;
-    ssl_bucket data = { (void*)buf, len };
+    SSL_BUCKET data = { (void*)buf, len };
 
     int i = ssl2_write_preflight(s);
     if (i <= 0)
@@ -480,7 +480,7 @@ int ssl2_write(SSL *s, const void *_buf, int len)
     }
 }
 
-static int write_pending(SSL *s, const ssl_bucket *buckets, int count, unsigned int len)
+static int write_pending(SSL *s, const SSL_BUCKET *buckets, int count, unsigned int len)
 {
     int i;
 
@@ -491,7 +491,7 @@ static int write_pending(SSL *s, const ssl_bucket *buckets, int count, unsigned 
      */
     if ((s->s2->wpend_tot > (int)len) ||
         (!(s->mode & SSL_MODE_ACCEPT_MOVING_WRITE_BUFFER) &&
-         !ssl_bucket_same(s->s2->wpend_bucket, s->s2->wpend_bucket_count, buckets, count) )) {
+         !SSL_BUCKET_same(s->s2->wpend_bucket, s->s2->wpend_bucket_count, buckets, count) )) {
         SSLerr(SSL_F_WRITE_PENDING, SSL_R_BAD_WRITE_RETRY);
         return (-1);
     }
@@ -522,7 +522,7 @@ static int write_pending(SSL *s, const ssl_bucket *buckets, int count, unsigned 
     }
 }
 
-static int n_do_ssl_write(SSL *s, const ssl_bucket *buckets, int count, int offset, int len)
+static int n_do_ssl_write(SSL *s, const SSL_BUCKET *buckets, int count, int offset, int len)
 {
     unsigned int j, k, olen, p, bs;
     int mac_size;
@@ -630,7 +630,7 @@ static int n_do_ssl_write(SSL *s, const ssl_bucket *buckets, int count, int offs
         return -1;
     }
     /* we copy the data into s->s2->wbuf */
-    ssl_bucket_cpy_out(s->s2->wact_data, buckets, count, offset, len);
+    SSL_BUCKET_cpy_out(s->s2->wact_data, buckets, count, offset, len);
     if (p)
         memset(&(s->s2->wact_data[len]), 0, p); /* arbitrary padding */
 
@@ -666,7 +666,7 @@ static int n_do_ssl_write(SSL *s, const ssl_bucket *buckets, int count, int offs
 
     /* lets try to actually write the data */
     s->s2->wpend_tot = olen;
-    memcpy(s->s2->wpend_bucket, buckets, sizeof(ssl_bucket)*count);
+    memcpy(s->s2->wpend_bucket, buckets, sizeof(SSL_BUCKET)*count);
     s->s2->wpend_bucket_count = count;
 
     s->s2->wpend_ret = len;
@@ -759,11 +759,11 @@ static int ssl_mt_error(int n)
 
 # ifndef OPENSSL_NO_IOVEC
 
-int ssl2_readv(SSL *s, const ssl_bucket *buckets, int count)
+int ssl2_readv(SSL *s, const SSL_BUCKET *buckets, int count)
 {
     return ssl2_read_internal(s, buckets, count, 0);
 }
-int ssl2_writev(SSL *s, const ssl_bucket *buckets, int count)
+int ssl2_writev(SSL *s, const SSL_BUCKET *buckets, int count)
 {
     unsigned int n, offset,len;
 
@@ -771,7 +771,7 @@ int ssl2_writev(SSL *s, const ssl_bucket *buckets, int count)
     if (i <= 0)
         return (i);
 
-    len = ssl_bucket_len(buckets, count);
+    len = SSL_BUCKET_len(buckets, count);
     if (len <= 0)
         return (len);
 

ssl/s2_srvr.c
@@ -537,7 +537,7 @@ static int decrypt_client_master_key(SSL *s)
         return -1;
     } else {
         SSL_EX_DATA_AKAMAI* ex_data = SSL_get_ex_data_akamai(s);
-        SSL_rsa_decrypt_ctx *ctx = &ex_data->task.ctx.rsa_decrypt;
+        SSL_RSA_DECRYPT_CTX *ctx = &ex_data->task.ctx.rsa_decrypt;
         ctx->src = &(p[s->s2->tmp.clear]);
         ctx->dest = &(p[s->s2->tmp.clear]);
         ctx->src_len = s->s2->tmp.enc;
@@ -547,7 +547,7 @@ static int decrypt_client_master_key(SSL *s)
 
         s->state = SSL2_ST_GET_CLIENT_MASTER_KEY_C;
         i = ssl_schedule_task(s, SSL_EVENT_KEY_EXCH_DECRYPT_DONE, ctx,
-                              (SSL_task_fn *)ssl_task_rsa_decrypt);
+                              (SSL_TASK_FN *)ssl_task_rsa_decrypt);
         if (i < 0) {
             ssl2_return_error(s,SSL2_PE_UNDEFINED_ERROR);
             SSLerr(SSL_F_GET_CLIENT_MASTER_KEY,SSL_R_DECRYPTION_FAILED);