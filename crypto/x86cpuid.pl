2f7dc7058a6cd699e5101bc434332ca42053bfc0
Benjamin Kaduk <bkaduk@akamai.com>
2016-10-03 15:15:06 -0500

    CR 3550391: Revert "Debian: Make assembly code PIC-compatible"
    This reverts commit 832bfdd7a52530895a726b48956db2326cf0fcbc.
    
    The Debian change was intended to fix build breakage when building
    shared libraries without -Bsymbolic (and when using assembly packs).
    However, it seems that upstream commit
    ddc20d4da9d770afeace2b4111d5d39d00e6c6b4 has provided a work-around
    for the build breakage for 5 years, and Debian did not update to remove
    their old patch.
    
    In any case, Akamai is building with -Bsymbolic via the defaults in
    Makefile.shared, so we would not be affected by the issue regardless.
    
    To be squashed into commit 832bfdd7a52530895a726b48956db2326cf0fcbc
    (so that both are removed) on rebase.

--- crypto/x86cpuid.pl
+++ crypto/x86cpuid.pl
@@ -8,8 +8,6 @@ require "x86asm.pl";
 
 for (@ARGV) { $sse2=1 if (/-DOPENSSL_IA32_SSE2/); }
 
-push(@out, ".hidden OPENSSL_ia32cap_P\n");
-
 &function_begin("OPENSSL_ia32_cpuid");
 	&xor	("edx","edx");
 	&pushf	();
@@ -157,7 +155,9 @@ push(@out, ".hidden OPENSSL_ia32cap_P\n");
 &set_label("nocpuid");
 &function_end("OPENSSL_ia32_cpuid");
 
-&function_begin_B("OPENSSL_rdtsc");
+&external_label("OPENSSL_ia32cap_P");
+
+&function_begin_B("OPENSSL_rdtsc","EXTRN\t_OPENSSL_ia32cap_P:DWORD");
 	&xor	("eax","eax");
 	&xor	("edx","edx");
 	&picmeup("ecx","OPENSSL_ia32cap_P");
@@ -171,7 +171,7 @@ push(@out, ".hidden OPENSSL_ia32cap_P\n");
 # This works in Ring 0 only [read DJGPP+MS-DOS+privileged DPMI host],
 # but it's safe to call it on any [supported] 32-bit platform...
 # Just check for [non-]zero return value...
-&function_begin_B("OPENSSL_instrument_halt");
+&function_begin_B("OPENSSL_instrument_halt","EXTRN\t_OPENSSL_ia32cap_P:DWORD");
 	&picmeup("ecx","OPENSSL_ia32cap_P");
 	&bt	(&DWP(0,"ecx"),4);
 	&jnc	(&label("nohalt"));	# no TSC
@@ -238,7 +238,7 @@ push(@out, ".hidden OPENSSL_ia32cap_P\n");
 	&ret	();
 &function_end_B("OPENSSL_far_spin");
 
-&function_begin_B("OPENSSL_wipe_cpu");
+&function_begin_B("OPENSSL_wipe_cpu","EXTRN\t_OPENSSL_ia32cap_P:DWORD");
 	&xor	("eax","eax");
 	&xor	("edx","edx");
 	&picmeup("ecx","OPENSSL_ia32cap_P");
