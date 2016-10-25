cb04bea56a4460dbb9461e0713e5edb416f78c73
Todd Short <tshort@akamai.com>
2016-10-25 17:53:37 -0400

    CR 3570631: Akamai X509 Enhancements
    Move Akamai's X509 code to new file x509_akamai.c
    Add X509_akamai_get_sha1_hash()
    Add X509_STORE_akamai_get_references()
    
    Original Author: tshort@akamai.com
    Code Reviewer: bkaduk@akamai.com
    Akamai Status: internal (for now)

--- crypto/x509/x509_d2.c
+++ crypto/x509/x509_d2.c
@@ -11,14 +11,6 @@
 #include "internal/cryptlib.h"
 #include <openssl/crypto.h>
 #include <openssl/x509.h>
-#ifndef OPENSSL_NO_AKAMAI
-# ifndef WIN32
-#  include <sys/uio.h>
-#  ifndef HAVE_STRUCT_IOVEC
-#   define HAVE_STRUCT_IOVEC
-#  endif
-# endif
-#endif
 
 int X509_STORE_set_default_paths(X509_STORE *ctx)
 {
@@ -63,28 +55,3 @@ int X509_STORE_load_locations(X509_STORE *ctx, const char *file,
         return (0);
     return (1);
 }
-
-
-#ifndef OPENSSL_NO_AKAMAI
-int X509_STORE_load_mem(X509_STORE *ctx, void *buf, int len)
-{
-# ifdef HAVE_STRUCT_IOVEC
-    X509_LOOKUP *lookup;
-    struct iovec iov;
-
-    lookup = X509_STORE_add_lookup(ctx, X509_LOOKUP_mem());
-    if (lookup == NULL)
-        return (0);
-
-    iov.iov_base = buf;
-    iov.iov_len = len;
-
-    if (X509_LOOKUP_add_mem(lookup, &iov, X509_FILETYPE_PEM) != 1)
-        return (0);
-
-    return (1);
-# else
-    return (0);
-# endif
-}
-#endif
