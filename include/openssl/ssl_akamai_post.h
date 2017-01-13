0584abba3f12079162cfffeb2e26d8565023b583
Todd Short <tshort@akamai.com>
2017-01-13 15:42:26 -0500

    CR 3633001: Add support for free/alloc buffers
    OpenSSL already has the feature of SSL_MODE_RELEASE_BUFFERS that can
    be set to release the read or write buffers when data has finished
    reading or writing. OpenSSL will automatically re-allocate the buffers
    as needed. This can be quite aggressive in terms of memory allocation.
    
    This provides a manual mechanism. SSL_akamai_free_buffers() will free
    the data buffers if there's no pending data. SSL_akamai_alloc_buffers()
    will realloc them; but this function is not strictly necessary, as it's
    still done automatically in the state machine.
    
    For mghost functionality.
    
    CR 3765001: Fix no-akamai builds
    
    Original Author: tshort@akamai.com
    Code Reviewer: bkaduk@akamai.com
    Akamai Status: PR #2240

--- include/openssl/ssl_akamai_post.h
+++ include/openssl/ssl_akamai_post.h
@@ -215,6 +215,9 @@ void AKAMAI_openssl_init_memory_stats(int lock);
 /* |cb| invoked for every structure type */
 void AKAMAI_openssl_get_memory_stats(void (*cb)(const AKAMAI_EX_DATA_STATS*, void*), void *param);
 
+int SSL_akamai_free_buffers(SSL *ssl);
+int SSL_akamai_alloc_buffers(SSL *ssl);
+
 /* Replaces SSL_CTX_sessions() and OPENSSL_LH_stats_bio() for shared session cache. */
 void SSL_CTX_akamai_session_stats_bio(SSL_CTX *ctx, BIO *b);
 
