bdbacc6f958b7ccae7dc2e365e8c6c8d9bc405c4
Todd Short <tshort@akamai.com>
2017-01-17 15:39:16 -0500

    CR 3635051: Access tlsext_ticket_expected
    Return the value of SSL->tlsext_ticket_expected
    
    Original Author: tshort@akamai.com
    Code Reviewer: bkaduk@akamai.com
    Akamai Status: internal

--- include/openssl/ssl_akamai_post.h
+++ include/openssl/ssl_akamai_post.h
@@ -250,6 +250,8 @@ void AKAMAI_openssl_get_memory_stats(void (*cb)(const AKAMAI_EX_DATA_STATS*, voi
 int SSL_akamai_free_buffers(SSL *ssl);
 int SSL_akamai_alloc_buffers(SSL *ssl);
 
+__owur int SSL_akamai_ticket_expected(const SSL *s);
+
 #  ifdef  __cplusplus
 }
 #  endif
