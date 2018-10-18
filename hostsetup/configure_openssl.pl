2ec01b8b0c7500166e0b4077e45bf6fa7c22718e
Todd Short <tshort@akamai.com>
2018-10-18 16:10:16 -0400

    CR 4135751: Remove openssl executable from bundle
    It gets pulled in via shared library
    
    Original Author: tshort@akamai.com
    Code Reviewer: bkaduk@akamai.com
    Akamai Status: internal only

--- hostsetup/configure_openssl.pl
+++ hostsetup/configure_openssl.pl
@@ -1,9 +1,12 @@
-
 package configure_openssl;
 
 use strict;
 use akamailog;
+use File::Basename;
 use config qw( $global_conf_data );
+
+my $varname = "openssl_executable";
+my $thiscomponent = "openssl";
 
 sub special {
     my ($installed_root, $temp_root, $request) = @_;
@@ -25,5 +28,45 @@ sub usereg {
     1;
 }
 
-1;
+sub configure {
+    my ($tmp_root, $host) = @_;
+    my $akamai = &config::akamai_dir();
+    my $bin = "$tmp_root/$akamai/bin";
+    my $hostobj = $config::global_conf_data->return_host($host);
+    my $variance = $config::global_conf_data->get_host_variance($hostobj, $varname);
 
+    if (-f "$bin/openssl") {
+        akamai_die(AKALOG_ERROR, 0, "$host:$thiscomponent: openssl exectuable already exists");
+    }
+
+    if (defined $variance) {
+        if ($variance !~ m/openssl[1-9][0-9][0-9]/) {
+            akamai_die(AKALOG_ERROR, 0, "$host:$thiscomponent: '$varname' variance '$variance', inalid value");
+        }
+        if (! -f "$bin/$variance") {
+            akamai_die(AKALOG_ERROR, 0, "$host:$thiscomponent: '$varname' variance '$variance', file not found");
+        }
+
+        akamai_log(AKALOG_INFO, 0, "$host:$thiscomponent: '$varname' variance '$variance', creating link");
+
+    } else {
+        # variance is undefined, check for an openssl executable
+        my @files = glob("$bin/openssl[1-9][0-9][0-9]*");
+
+        $variance = shift(@files);
+
+        if (! defined $variance) {
+            akamai_die(AKALOG_ERROR, 0, "$host:$thiscomponent: '$varname' variance does not exist, cannot find exectuable");
+        }
+
+        # strip the path name so we can use it below
+        $variance = basename($variance);
+
+        akamai_log(AKALOG_INFO, 0, "$host:$thiscomponent: '$varname' variance does not exist, creating link to '$variance'");
+    }
+
+    main::log_symlink("./$variance", "$bin/$thiscomponent");
+    1;
+}
+
+1;
